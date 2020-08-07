97eb0c33ef57af3aa994ea144a4d29d364a2bd3b
Arsen Poghosyan <arsenpoghosyan1989@gmail.com>
2020-08-07 20:07:01 +0400

    Updates

---
+++ precache-manifest.b56e7773e6674c125b7fbd0b34ad7e62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d6044c109369592058cb66f9b006fd",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "ad1caf1e84111a235564",
+    "url": "/TicTacToe/static/css/main.af2f978d.chunk.css"
+  },
+  {
+    "revision": "bd4575fd6deaf674c73b",
+    "url": "/TicTacToe/static/js/2.05fdfedb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.05fdfedb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad1caf1e84111a235564",
+    "url": "/TicTacToe/static/js/main.5cc521e5.chunk.js"
+  },
+  {
+    "revision": "620067a55f118248c6e9",
+    "url": "/TicTacToe/static/js/runtime-main.dabf51b0.js"
+  }
+]);
